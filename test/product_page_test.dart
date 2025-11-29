fc4d2fcece6a148e8027491949707f72e37d387b
Ghaith05 <ghaithahmadalsawair73@gmail.com>
2025-11-29 19:04:05 +0000

    edit code to have proper test

--- test/product_page_test.dart
+++ test/product_page_test.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/data/sample_data.dart';
 import 'package:union_shop/views/product_page.dart';
 
 void main() {
   testWidgets('ProductPage placeholder renders', (WidgetTester tester) async {
-    await tester.pumpWidget(
-      MaterialApp(home: const ProductPage()),
-    );
+   await tester.pumpWidget(MaterialApp(
+  home: ProductPage(product: sampleProducts.first),
+));
 
     expect(find.text('Product'), findsOneWidget); // app bar title
     expect(find.text('Product detail / list placeholder'), findsOneWidget);
