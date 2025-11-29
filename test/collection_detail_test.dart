16b464be7b8884c25bde253ec51dceaba0408a2f
Ghaith05 <ghaithahmadalsawair73@gmail.com>
2025-11-29 19:03:06 +0000

    edit test to import collectionpage insetad of collectionspage

--- test/collection_detail_test.dart
+++ test/collection_detail_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:union_shop/views/collections_page.dart';
+import 'package:union_shop/views/collection_page.dart';
 import 'package:union_shop/data/sample_data.dart';
 
 void main() {
@@ -10,7 +10,7 @@ void main() {
 
     await tester.pumpWidget(
       MaterialApp(
-        home: CollectionDetailPage(collection: collection),
+        home: CollectionPage(collection: collection),
       ),
     );
 
