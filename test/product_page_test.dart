ca4dcb1c544a655b44293a32b47836eed00d0fc5
Ghaith05 <ghaithahmadalsawair73@gmail.com>
2025-11-30 21:33:35 +0000

    add tests for size, color and qty

--- test/product_page_test.dart
+++ test/product_page_test.dart
@@ -25,4 +25,48 @@ void main() {
     expect(find.widgetWithText(ElevatedButton, 'Add to cart'), findsOneWidget);
     expect(find.widgetWithText(OutlinedButton, 'Share'), findsOneWidget);
   });
+  
+    testWidgets('ProductPage dropdowns and qty selector work; actions disabled',
+      (WidgetTester tester) async {
+    final prod = sampleProducts.first;
+
+    await tester.pumpWidget(MaterialApp(home: ProductPage(product: prod)));
+    await tester.pumpAndSettle();
+
+    // Initial hint texts present
+    expect(find.text('Size'), findsOneWidget);
+    expect(find.text('Color'), findsOneWidget);
+    expect(find.text('Qty'), findsOneWidget);
+
+    // Select Size -> 'M'
+    await tester.tap(find.text('Size').last);
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('M').last);
+    await tester.pumpAndSettle();
+    expect(find.text('M'), findsWidgets);
+
+    // Select Color -> 'Blue'
+    await tester.tap(find.text('Color').last);
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('Blue').last);
+    await tester.pumpAndSettle();
+    expect(find.text('Blue'), findsWidgets);
+
+    // Select Qty -> '3'
+    await tester.tap(find.text('Qty').last);
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('3').last);
+    await tester.pumpAndSettle();
+    expect(find.text('3'), findsWidgets);
+
+    // Buttons should be present but disabled (onPressed == null)
+    final ElevatedButton addBtn = tester.widget(find.widgetWithText(ElevatedButton, 'Add to cart'));
+    expect(addBtn.onPressed, isNull);
+
+    final ElevatedButton buyBtn = tester.widget(find.widgetWithText(ElevatedButton, 'Buy with shop'));
+    expect(buyBtn.onPressed, isNull);
+
+    final OutlinedButton shareBtn = tester.widget(find.widgetWithText(OutlinedButton, 'Share'));
+    expect(shareBtn.onPressed, isNull);
+  });
 }
