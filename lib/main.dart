6a515a902593db6280a1c8093d0ccd74d34579e3
Ghaith05 <ghaithahmadalsawair73@gmail.com>
2025-12-04 01:26:15 +0000

    update main.dart to register /search route

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:union_shop/views/account_dashboard.dart';
 import 'package:union_shop/widgets/navbar.dart';
 import 'package:union_shop/views/cart_page.dart';
 import 'package:union_shop/views/print_shack.dart';
+import 'package:union_shop/views/search_results_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -54,6 +55,7 @@ class UnionShopApp extends StatelessWidget {
         AccountDashboard.routeName: (context) => const AccountDashboard(),
         CollectionsPage.routeName: (context) => const CollectionsPage(),
         CartPage.routeName: (context) => const CartPage(),
+        SearchResultsPage.routeName: (context) => const SearchResultsPage(),
       },
     );
   }
